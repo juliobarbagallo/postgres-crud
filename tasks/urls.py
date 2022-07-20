b0dbe8b03f5e7ff3c4a63487e0bb444ff4a9ade0
Julio Barbagallo <julio.barbagallo@gmail.com>
2022-07-20 14:26:18 -0300

    Adds cors middleware and sabe to Tasks method

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_tasks
+from .views import create_task, list_tasks
 
 urlpatterns = [
     path('', list_tasks, name='list_tasks'),
+    path('add/', create_task, name='create_tasks'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 
 
 def list_tasks(request):
     return render(request, 'list_tasks.html')
+
+
+def create_task(request):
+    # print(request.POST)
+    Task(title=request.POST['title'],
+         description=request.POST['description']).save()
+    return redirect('list_tasks')
